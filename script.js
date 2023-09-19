09bb03e902e31a648688a39e372b140e06beb18b
Hauptmannhuy <dindago2324@gmail.com>
2023-09-20 01:58:41 +0300

    test

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const buttons = document.querySelectorAll('button');
 let output = document.getElementById('display');
 let displayValue;
-let firstNumber = null;
-let secondNumber = null;
-let operand = null;
-
+let firstNumber='';
+let secondNumber='';
+let operator='';
+let operandStatus = false;
+let result;
 function add(a,b){
 return a+b;
 }
@@ -21,71 +22,115 @@ function divide(a,b){
 }
 
 function operate(operator,a,b){
-
+    //pseudocode
+    //if operator === + then function add()
+    // if operator === / then function divide()
 }
 
 buttons.forEach((button)=>{
     button.addEventListener('click', (e)=>{
         let target = e.target;
+       
+       
         if (target.id === 'one-Btn'){
-            displayValue += "1";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='1';
+            }
+            else {
+                firstNumber+='1';
+            }
         }
         if (target.id === 'two-Btn'){
-            displayValue += "2";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='2';
+            }
+            else {
+                firstNumber+='2';
+            }
         }
         if (target.id === 'three-Btn'){
-            displayValue += "3";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='3';
+            }
+            else {
+                firstNumber+='3';
+            }
         }
         if (target.id === 'four-Btn'){
-            displayValue += "4";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='4';
+            }
+            else {
+                firstNumber+='4';
+            }
         }
         if (target.id === 'five-Btn'){
-            displayValue += "5";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='5';
+            }
+            else {
+                firstNumber+='5';
+            }
         }
         if (target.id === 'six-Btn'){
-            displayValue += "6";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='6';
+            }
+            else {
+                firstNumber+='6';
+            }
         }
         if (target.id === 'seven-Btn'){
-            displayValue += "7";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='7';
+            }
+            else {
+                firstNumber+='7';
+            }
         }
         if (target.id === 'eight-Btn'){
-            displayValue += "8";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='8';
+            }
+            else {
+                firstNumber+='8';
+            }
         }
         if (target.id === 'nine-Btn'){
-            displayValue += "9";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='9';
+            }
+            else {
+                firstNumber+='9';
+            }
         }
         if (target.id === 'zero-Btn'){
-            displayValue += "0";
-            output.textContent = displayValue;
+            if (operandStatus === true){
+                secondNumber+='0';
+            }
+            else {
+                firstNumber+='1';
+            }
         }
         if (target.id === 'plus-Btn'){
-            displayValue += "+";
-            output.textContent = displayValue;
+            operandStatus = true;
+            operator = '+'
         }
         if (target.id === 'minus-Btn'){
-            displayValue += "-";
-            output.textContent = displayValue;
+            operandStatus = true;
+            operator = '-'
         }
         if (target.id === 'divide-Btn'){
-            displayValue += "/";
-            output.textContent = displayValue;
+            operandStatus = true;
+            operator = '/'
         }
         if (target.id === 'multiply-Btn'){
-            displayValue += "*";
-            output.textContent = displayValue;
+            operandStatus = true;
+            operator = '*'
         }
         if (target.id === 'equal-Btn'){
-            displayValue += "=";
-            output.textContent = displayValue;
+            operandStatus = false;
+            operate(operator,firstNumber,secondNumber);
         }
     })
 })
